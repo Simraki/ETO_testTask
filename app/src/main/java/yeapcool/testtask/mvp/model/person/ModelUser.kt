ac38ca3c3085a06b7106bda93a625adb24b5cfc2
YeapCool <Simraki@users.noreply.github.com>
2017-12-04 20:07:38 +0700

    Update ModelUser.kt

--- app/src/main/java/yeapcool/testtask/mvp/model/person/ModelUser.kt
+++ app/src/main/java/yeapcool/testtask/mvp/model/person/ModelUser.kt
@@ -6,27 +6,21 @@ object ModelUser {
     private var personMy: Person? = null
     private var isMy: Boolean = false
 
-    @Synchronized
     fun saveUser(p: Person?) {
         personUser = p
     }
 
-    @Synchronized
     fun getUser(): Person? = personUser
 
-    @Synchronized
     fun saveMy(p: Person?) {
         personMy = p
     }
 
-    @Synchronized
     fun getMy(): Person? = personMy
 
-    @Synchronized
     fun setIsMy(b: Boolean) {
         isMy = b
     }
 
-    @Synchronized
     fun isMy(): Boolean = isMy
 }
